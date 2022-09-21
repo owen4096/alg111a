647f0d2833b64ff07bce827b1ccd2624f8791497
ccckmit <ccckmit@gmail.com>
2022-09-21 09:49:20 +0800

    reorganize markov

--- 03-monteCarlo/01-pi/buffonNeedlePi.py
+++ 03-monteCarlo/01-pi/buffonNeedlePi.py
@@ -2,7 +2,7 @@
 
 from numpy.random import uniform
 import numpy as np
-t=45; L=36; n=5000
+t=45; L=36; n=50000
 x=uniform(0,t/2,n) # n 個 [0,t/2] 上的隨機數
 phi = uniform(0, np.pi, n)
 h = sum(x<=L*np.sin(phi)/2)
