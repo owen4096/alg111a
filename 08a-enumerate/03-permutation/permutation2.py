c0c6c441eb2404ee4ac3dae1a3198027ecb3d35d
ji3k27au/6y7 <owen4096@gmail.com>
2022-11-02 19:36:36 +0800

    permutation-fail-2

--- 08a-enumerate/03-permutation/permutation2.py
+++ 08a-enumerate/03-permutation/permutation2.py
@@ -7,13 +7,13 @@ def cnk(n,k): # 主函數
 	y=1
 	return permNext(n,k,p,y) # 呼叫 permNext 遞迴下去排出所有可能
 
-def permNext(n,k,p,y): # 已經排好 p[0..i-1], 繼續排下去 [i...n-1]
+def permNext(n,k,p,y): 
 	count=0
 	global count2
 	global dic
 	i = len(p)
 	if i == k:
-		if not p in dic:
+		if not p in dic:  #確認是否在字典裡 否則放入並印出 是則do nothing
 			dic[count2]=p
 			count2=count2+1
 			
@@ -21,19 +21,16 @@ def permNext(n,k,p,y): # 已經排好 p[0..i-1], 繼續排下去 [i...n-1]
 		
 		return	
 		
-	for x in range(y,n+1):
-		permNext(n,k,p,y+1)
+	for x in range(y,n+1):  #因為組合沒有順序性 因此我的想法為只找由小到大 則為全部組合
+		permNext(n,k,p,y+1) #每個數字都可以選擇不放或放 此行為不放
 		if x not in p:
-			for j in range(len(p)):
-				if x > p[j]:
-					count=count+1
+			for j in range(len(p)):  #判斷x是否大於p中所有的數字 是則放入
+				if x > p[j]:		 #判斷x是否大於p中所有的數字 是則放入
+					count=count+1	 #判斷x是否大於p中所有的數字 是則放入
 			if count == len(p):
-				p.append(x)    # 把 x 放進盤面
-				permNext(n,k,p,y+1) # 繼續遞迴尋找下一個排列
-				p.pop()       
-		
-
-						        
+				p.append(x)    
+				permNext(n,k,p,y+1) 
+				p.pop()       					        
 		
 cnk(6,3) 
 
